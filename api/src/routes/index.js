333a9264abc5417b140849f922aa248903e2a3f1
zaratush <martinc.1@hotmail.com>
2021-03-12 12:58:10 -0300

    create category,create product

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,9 +1,11 @@
 const { Router } = require("express");
-const getProducts = require("../controllers/products/get.products.js");
 const getProductsRouter = require("./products/get.products.js");
-
+const postProductsRouter = require("./products/post.products.js");
+const postCategoriesRouter=require("./categories/post.categories")
 const router = Router();
 
 router.use("/products", getProductsRouter);
+router.use("/products", postProductsRouter);
+router.use("/categories",postCategoriesRouter );
 
 module.exports = router;
