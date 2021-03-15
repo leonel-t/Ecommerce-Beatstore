88cc33baab0d502e2d220a953e56c50247a19b86
Matias Flores <floresmatias0@gmail.com>
2021-03-15 19:43:28 -0300

    DELETE-OLD-HEADER

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,36 +1,39 @@
 import React from 'react';
-import Logo from "./logo.svg"
-import { Link } from 'react-router-dom';
-import SearchBar from './SearchBar'
 import "./Header.css"
-
-
-const Header = () =>{
+import Logo from '../../assets/images/logo.png'
+import { Link } from 'react-router-dom';
+const NewHeader = () =>{
 
 
     return (
-      <div className="NavBar" >
-
-        <img className="navbar-zCOIAP" data-id="342fc321-f2b1-4e94-a2f0-d97f2118bd31" src="https://cdn.animaapp.com/projects/604829c37b81d727e7cb8c9e/releases/6048e9a37c95ed32e1c01cfc/img/navbar@1x.png" anima-src="https://cdn.animaapp.com/projects/604829c37b81d727e7cb8c9e/releases/6048e9a37c95ed32e1c01cfc/img/navbar@1x.png" alt="background"/>
-
-        <div id="logo"> 
-          <Link to="/" id="logoLink">
-            <img src={Logo}  alt="logo"/>
-          </Link> 
-        </div>
-
-        <SearchBar />
-        <div className="navOptions">
-            <ul> 
-              <Link to="/" ><li id="option1">option 1</li></Link>
-              <Link to="/" ><li id="option2">option 2</li></Link>
-              <Link to="/" ><li id="option3">option 3</li></Link>
-            </ul> 
-        </div>
-      </div>
+      <header className="--newHeader-main" >
+          <div className="--newHeader-main-row">
+            <div className="--newHeader-main-row-col-logo">
+                <Link to="/"><img src={Logo} alt="BeatShop"></img></Link>
+            </div>
+            <div className="--newHeader-main-row-col-menu">
+                <ul className="--newHeader-main-row-col-menu-ul">
+                    <li><Link className="--newHeader-main-row-col-menu-link" to="/">Home</Link></li>
+                    <li><Link className="--newHeader-main-row-col-menu-link" to="/catalog">Catalog</Link></li>
+                    <li><Link className="--newHeader-main-row-col-menu-link" to="/login">Login</Link></li>
+                    <li><Link className="--newHeader-main-row-col-menu-link" to="/add">Admin</Link></li>
+                </ul>
+            </div>
+            <div className="--newHeader-main-row-col-user">
+                <div className="--newHeader-main-row-col-user-icon">
+                    <span class="material-icons --user-icon">
+                    account_circle
+                    </span>
+                </div>
+                <div className="--newHeader-main-row-col-user-options">
+                    <p>Login / Register</p>
+                </div>
+            </div>
+          </div>
+      </header>
     )
 }
 
 
 
-export default Header;
+export default NewHeader;
