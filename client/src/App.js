c4769ec27e8dc7227754046095deb1789f609763
leonel-t <tomasini.leonel@gmail.com>
2021-03-15 00:20:55 -0300

    login page created

--- client/src/App.js
+++ client/src/App.js
@@ -8,11 +8,13 @@ import Catalog from "./pages/Catalog.js";
 import Admin from "./pages/Admin.js";
 import Form from "./pages/Form";
 import PutForm from "./pages/PutForm";
+import Login from "./pages/Login.js";
 
 //Components
 //import Header from './components/Header/Header.js';
 import Header from "./components/NewHeader/NewHeader.js";
 import Footer from "./components/Footer/Footer.js";
+
 //import FloatingCard from './components/FloatingCard/FloatingCard.js';
 
 const App = () => {
@@ -26,6 +28,7 @@ const App = () => {
         <Route exact path="/add" render={() => <Form />} />
         <Route exact path="/admin" render={() => <Admin />} />
         <Route exact path="/put/:id" render={() => <PutForm />} />
+        <Route exact path="/login" render={() => <Login/>}/>
       </Switch>
 
       <Footer />
