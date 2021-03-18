88e4445ae304c144f5295b012f95713f266a4f17
Matias Flores <floresmatias0@gmail.com>
2021-03-18 02:43:57 -0300

    LOGOUT-START

--- client/src/pages/Auth/Profile.js
+++ client/src/pages/Auth/Profile.js
@@ -1,21 +1,40 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import {fetchUser} from '../../stores/user/user.actions'
+import {useHistory} from 'react-router-dom'
+
 import './Profile.css'
 
 const Profile = ({fetchUserEffect, STORE_USER}) =>{
+
+  const history = useHistory();
 
     useEffect(() => {
         fetchUserEffect();
       }, [fetchUserEffect]);
 
+      const handleClick = (e) =>{
+        e.preventDefault()
+        localStorage.clear()
+        history.push("/")
+      }
+      const handleClick2 = (e) =>{
+        e.preventDefault()
+        history.push("/login")
+      }
+
+
     return (
         <div>
             {STORE_USER.user && STORE_USER.user.data
               ?(
-                <h1>Profile: {"  "} {STORE_USER.user.data.user.name}</h1>
+                <div>
+                   <h1 className="titule">Profile: {"  "} {STORE_USER.user.data.user.name}</h1>
+                   <button onClick={handleClick}>Logout</button>
+                </div>
               ):(
-                <h1>User Not Found</h1>
+                <h1>User Not Found </h1>,
+                <button onClick={handleClick2}>Go to login</button>
               )  
             }
             
