68a6c0a513db29bc84287250a9d933f3f486fe7c
unknown <lucasdelye@gmail.com>
2021-03-18 13:55:39 -0300

    Delete/Put Cateries Router Finished

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,9 +2,12 @@ const { Router } = require("express");
 const getProductsRouter = require("./products/get.products.js");
 const postProductsRouter = require("./products/post.products.js");
 const deleteProductsRouter = require("./products/delete.products.js");
-const postCategoriesRouter = require("./categories/post.categories");
 const putProductsRouter = require("./products/put.products");
+
 const getCategoriesRouter = require("./categories/get.categories");
+const postCategoriesRouter = require("./categories/post.categories");
+const putCategoriesRouter = require("./categories/put.categories");
+const deleteCategoriesRouter = require("./categories/delete.categories");
 
 const getUserRouter = require("./users/get.users");
 const postUserRouter = require("./users/post.users");
@@ -20,6 +23,9 @@ router.use("/products", putProductsRouter);
 
 router.use("/categories", postCategoriesRouter);
 router.use("/categories", getCategoriesRouter);
+router.use("/categories", putCategoriesRouter);
+router.use("/categories", deleteCategoriesRouter);
+
 
 router.use("/users", getUserRouter);
 router.use("/users", postUserRouter);
