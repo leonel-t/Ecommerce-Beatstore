a31b0788137c6e655c04eab73f54a271f0265ab3
leonel-t <tomasini.leonel@gmail.com>
2021-03-19 01:01:16 -0300

    Update card catalog

--- client/src/components/Product/Catalog/CatalogCard.js
+++ client/src/components/Product/Catalog/CatalogCard.js
@@ -1,21 +1,52 @@
 import React from "react";
 import "./CatalogCard.css";
 import {Link} from 'react-router-dom';
+import { connect } from "react-redux";
+import { addItemToCart } from "../../../stores/user/user.actions";
+import { show, ACTION_TYPE } from 'js-snackbar';
 
-
-const CatalogCard = ({id, name, autor, image}) => {
-
+const CatalogCard = ({addItemToCartEffect,STORE_PRODUCTS, id, name, autor, image, price, product}) => {
+    const handleAddToCart = (product)=>{
+        show({ text: 'PRODUCT ADDED!', pos:'bottom-center', duration: 5000, });
+        return addItemToCartEffect("user", product)
+    }
     return (
         <div className="--CatalogCard">
             <div className="--CatalogCard-image">
                 <img alt="albumimage" src={`http://localhost:3001/images/${image}`}/>
             </div>
-            <div className="--CatalogCard-info">
-                <p className="--CatalogCard-info-song"><Link className="--CatalogCard-link" to={`/product/${id}`}>{name}</Link></p>
-                <p className="--CatalogCard-info-autor">{autor}</p>
+            <div className="--CatalogCard-details">
+                <div className="--CatalogCard-info">
+                    <p className="--CatalogCard-info-song">
+                        <Link className="--CatalogCard-link" to={`/product/${id}`}>
+                            {name.length > 20
+                                ?(
+                                    name.slice(0,20) + "..."
+                                ):(
+                                    name
+                                )
+                            }
+                        </Link></p>
+                    <p className="--CatalogCard-info-autor">{autor}</p>
+                </div>
+                <div className="--CatalogCard-details-price">
+                    <p>$ {price}</p>
+                    <span onClick={()=> {return handleAddToCart(product)} } className="material-icons --CatalogCard-details-shopIcon">add_shopping_cart</span>
+                </div>
             </div>
+            
         </div>
     )
 }
-
-export default CatalogCard;
+const mapStateToProps = (state) => {
+    return {
+      STORE_PRODUCTS: state.productsReducers,
+    };
+  };
+  const mapDispatchToProps = (dispatch) => {
+    return {
+        addItemToCartEffect: (user, product) => dispatch(addItemToCart(user, product)),
+    };
+  };
+  
+  export default connect(mapStateToProps, mapDispatchToProps)(CatalogCard);
