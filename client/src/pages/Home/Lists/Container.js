6a9eac10b70091956621221091a5c37dcd0399ee
Feli8712 <felipe.traina.azul@gmail.com>
2021-03-21 07:58:19 -0300

    HOME WRAPPER 1

--- client/src/pages/Home/Lists/Container.js
+++ client/src/pages/Home/Lists/Container.js
@@ -3,15 +3,17 @@ import React from 'react';
 import './Container.css'
 import Cards from "./Cards"
 import CardsTres from "./Cards3"
+import Wrapper from "../wrapper/Wrapper"
 
 
 const Container = () => {
     return (
         <div className='Container'>
-            <h2 id="title1">Most Popular</h2>
-                <Cards/>
-            <h2 id="title2">Recommends</h2>
-                <CardsTres/>
+                <Wrapper/>
+                <Wrapper/>
+                <Wrapper/>
+                <Wrapper/>
+                <Wrapper/>
         </div>
         )
 }
---
+++ client/src/pages/Home/wrapper/Wrapper.js
@@ -0,0 +1,96 @@
+import React, {useEffect, useState, useRef} from 'react';
+import {animated, useSpring } from "react-spring";
+import { useScroll } from "react-use-gesture"
+import { connect } from 'react-redux';
+import {useHistory} from 'react-router-dom';
+import { fetchAllProducts } from '../../../stores/products/products.actions';
+
+import "./wrapper.css";
+
+const Product = ({fetchAllProductsEffect,  PRODUCTS}) =>{
+    const history = useHistory()
+    useEffect(()=>{
+        fetchAllProductsEffect()
+      },[fetchAllProductsEffect]);
+      const [styles, set] = useSpring(()=>({
+
+          transform:"perspective(500px) rotateY(25deg)"
+      }));
+      const clamp = (value, clampAt = 30) =>{
+          if(value > 0){
+              return value > clampAt ? clampAt : value
+          }else{
+            return value < -clampAt ? -clampAt : value
+          }
+      }
+
+      const bind = useScroll(event =>{
+          set({
+              transform:`perpective(500px) rotateY${
+                  event.scrolling ? clamp(event.delta[0]) : 0
+                }deg`
+          })
+      })
+      const handleClick = (id)=>{
+          history.push(`/product/${id}`)
+      }
+    return (
+        <>
+          {PRODUCTS.productLoading
+            ?(  
+            <div>
+                <p>Cargando</p>
+            </div>
+            ):(
+                <>
+                {PRODUCTS && PRODUCTS.length > 0
+                    ?(
+                        <div 
+                        {...bind()}
+                        className="wrapper">
+                            {
+                                PRODUCTS.map((product, index)=>{
+                                    let bg = `http://localhost:3001/images/${product.image}`
+                                    return (
+                                    <animated.div 
+                                    onClick={()=> handleClick(product.id)}
+                                    style={ {
+                                        ...styles,
+                                        cursor:`pointer`,
+                                        backgroundImage:`url(${bg})`}
+                                     }
+                                    className="item"> 
+                                      
+                                      
+                                    </animated.div>
+                                    )
+                                })
+                            }
+                        </div>
+                    ):(
+                        <div className="load">
+                    
+                        </div>
+                    )
+                }
+                </>
+            )
+          }
+        </>
+    )
+}
+
+const mapStateToProps =  state => {
+    return {
+      PRODUCTS : state.productsReducers.products,
+    }
+  }
+  const mapDispatchToProps = dispatch =>{
+    return {
+        fetchAllProductsEffect: () => dispatch(fetchAllProducts()),
+
+    }
+  }
+  
+  
+export default connect(mapStateToProps, mapDispatchToProps)(Product);
