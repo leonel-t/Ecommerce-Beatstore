2ef2be2e6c0ffaeebf0a690c1c459728ff095270
zaratush <martinc.1@hotmail.com>
2021-03-24 20:38:24 -0300

    search by category

--- api/src/controllers/products/get.products.js
+++ api/src/controllers/products/get.products.js
@@ -3,7 +3,7 @@ const Sequelize = require("sequelize");
 const Op = Sequelize.Op;
 
 module.exports = {
-  getAllProducts: async ()=> {
+  getAllProducts: async () => {
     return await Product.findAll({
       include: [
         {
@@ -31,8 +31,8 @@ module.exports = {
   },
   findProductById: async (id) => {
     return await Product.findOne({
-      where:{
-        id:id
+      where: {
+        id: id
       },
       include: [
         {
@@ -46,7 +46,7 @@ module.exports = {
       ],
     });
   },
-  getProductsByLetterIncludeInTheName: async (inputValue)=> {
+  getProductsByLetterIncludeInTheName: async (inputValue) => {
     return await Product.findAll({
       where: {
         name: {
@@ -55,5 +55,47 @@ module.exports = {
       },
       include: [{ model: Categories, as: "categories" }],
     });
+  },
+  searchProductsByCategoryName: (categories) => {
+    var products = [];
+    var categorias = [];
+
+    for (let i = 0; i < categories.length; i++) {
+      console.log("BUCLE", categories[i])
+      Categories.findAll({
+        where: {
+          name: {
+            [Op.iLike]: categories[i]
+          }
+        },
+        include: [{ model: Product, as: 'products' }]
+      }).then(cat => {
+        console.log(cat)
+
+        /*
+                for (let e = 0; e < cat[0].categories.length; e++) {
+                  if (cat[0].categories[e]) {
+        
+                    console.log(cat[0].categories[e])
+                  }
+                }
+        * */
+
+      })
+
+    }
+
+
+    /*
+        for (let e = 0; e < categorias[0].categories.length; e++) {
+          if (categorias[0].categories[e]) {
+            products.push(categorias[0].categories[e])
+          }
+           }
+    */
+
+
+    console.log("BUCLE", products)
+    return products
   },
 };
--- api/src/routes/products/get.products.js
+++ api/src/routes/products/get.products.js
@@ -3,40 +3,46 @@ const {
   getAllProducts,
   findProductById,
   findProductsByCategoryName,
-  getProductsByLetterIncludeInTheName
- } = require("../../controllers/products/get.products");
+  getProductsByLetterIncludeInTheName,
+  searchProductsByCategoryName,
+} = require("../../controllers/products/get.products");
 
 server.get("/", (req, res, next) => {
   return getAllProducts().then((products) => {
-      res.status(200).json(products);
-    }).catch((error) => {
-      res.status(400).json(error);
-    });
+    res.status(200).json(products);
+  }).catch((error) => {
+    res.status(400).json(error);
+  });
 });
 server.get("/:id", (req, res, next) => {
   const { id } = req.params;
-   return findProductById(id).then((product) => {
-     console.log(product)
-      res.status(200).json(product);
-    }).catch((error) => {
-      res.status(400).json(error);
-    });
+  return findProductById(id).then((product) => {
+    console.log(product)
+    res.status(200).json(product);
+  }).catch((error) => {
+    res.status(400).json(error);
+  });
 });
 server.get("/categoria/:nombreCat", (req, res, next) => {
   let { category } = req.params;
   return findProductsByCategoryName(category).then((products) => {
-      res.status(200).json(products);
-    }).catch((error) => {
-      res.status(400).json(error);
-    });
+    res.status(200).json(products);
+  }).catch((error) => {
+    res.status(400).json(error);
+  });
 });
 server.get("/search/:query", (req, res, next) => {
-    let { query } = req.params;
-    return getProductsByLetterIncludeInTheName(query).then((product) => {
-      res.status(200).json(product);
-    }).catch((error) => {
-      res.status(400).json(error);
-    });
+  let { query } = req.params;
+  return getProductsByLetterIncludeInTheName(query).then((product) => {
+    res.status(200).json(product);
+  }).catch((error) => {
+    res.status(400).json(error);
+  });
 });
+server.get("/categories/products", (req, res, next) => {
+  let { categories } = req.body
+  return searchProductsByCategoryName(categories)
+
+})
 
 module.exports = server;
