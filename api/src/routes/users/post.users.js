3ca957ecd63f3e11e732f2399b0e49e33d83307f
Feli8712 <felipe.traina.azul@gmail.com>
2021-03-24 08:43:24 -0300

    REFRESH TOKEN ADDED

--- api/src/routes/users/post.users.js
+++ api/src/routes/users/post.users.js
@@ -47,9 +47,8 @@ server.post("/",
 
   });
 function generateToken(user){
-    return jwt.sign({id: user.id, rol: user.rol}, ACCESS_TOKEN_SECRET,{expiresIn: "10s"});
+    return jwt.sign({id: user.id, rol: user.rol}, ACCESS_TOKEN_SECRET,{expiresIn: "10m"});
 }
-var refreshTokens = [];
 
 server.post('/token', function(req, res, next){
   const {useremail, refreshToken} = req.body;
@@ -59,10 +58,18 @@ server.post('/token', function(req, res, next){
       email:useremail
     }
   }).then(user =>{
+    const newRefreshToken = jwt.sign({id: user.id,rol: "refreshToken"}, REFRESH_TOKEN_SECRET);
 
-    refreshTokens = refreshTokens.filter((token)=> token !== refreshToken);
-    const newRefreshToken = jwt.sign({id: user.id,rol: user.rol}, REFRESH_TOKEN_SECRET);
-    refreshTokens.push(newRefreshToken)
+    User.update({
+      refresToken:newRefreshToken
+    },{
+      where:{
+        id: user.dataValues.id
+      }
+    }).catch((err)=>{
+      console.log(error)
+    })
+ 
     const newToken = generateToken({id: user.id,rol: user.rol})
   
     res.status(201).json({newToken, newRefreshToken})
@@ -84,8 +91,7 @@ server.post(
           req.logIn(user, { session: false }, function (err) {
             if (err) return next(err);
             const token = generateToken(user)
-            const refreshToken = jwt.sign({id: user.id,rol: user.rol}, REFRESH_TOKEN_SECRET);
-            refreshTokens.push(refreshToken)
+            const refreshToken = jwt.sign({id: user.id,rol: "refreshToken"}, REFRESH_TOKEN_SECRET);
             const dataUser = {
               id: user.id,
               email: user.email,
@@ -93,6 +99,15 @@ server.post(
               rol: user.rol,
             };
             const data = { msg: "Login successful", user: dataUser, token, refreshToken };
+            User.update({
+              refresToken:refreshToken
+            },{
+              where:{
+                id: user.dataValues.id
+              }
+            }).catch((err)=>{
+              console.log(error)
+            })
             return res.status(200).json(data);
           });
         }
