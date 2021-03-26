1a0055d0bcaba7b23b52257de4e33c8c96d9c6d5
Feli8712 <felipe.traina.azul@gmail.com>
2021-03-26 00:41:36 -0300

    SOUND LOGOUT

--- client/src/components/ProfileCard/ProfileCard.js
+++ client/src/components/ProfileCard/ProfileCard.js
@@ -1,12 +1,20 @@
 import React from "react"
 import "./ProfileCard.css"
 import profileImg from "../../assets/images/profile-image.jpg"
+import sound from "../../assets/audio/system-shut-down.mp3"
 
 const ProfileCard = ({ name, email }) => {
+
+const audio = new Audio(sound);
+      audio.volume=0.1;
+
     const handleLogout = (e) => {
         e.preventDefault()
+        audio.play()
         localStorage.clear()
+       setTimeout(()=>{
         return window.location.replace("http://localhost:3001/logout")
+       },1500)
     }
     return (
         <div className="--profileCard-user">
