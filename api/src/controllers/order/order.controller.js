3ddaca530f8c684f5c0a79df69689d926d5ccd2c
Feli8712 <felipe.traina.azul@gmail.com>
2021-03-27 21:29:56 -0300

    GET CART BY USER ID AND AUTO CREATE IF THE USER DONT HAVE A ORDER WITCH STATUS CART

--- api/src/controllers/order/order.controller.js
+++ api/src/controllers/order/order.controller.js
@@ -31,4 +31,65 @@ module.exports = {
       }
     ).then((order) => order);
   },
+  getOrdersByUserID : async (userId, orderStatus) => {
+    var newUserId = userId;
+    if(orderStatus === "all"){
+      return await Order.findAll({
+          where:{
+            userId:userId
+          },
+          include:[
+            {
+              model: OrderLine,
+            },
+          ]
+        }).then((orders)=>{
+          return orders
+        }).catch((error)=>{
+          return error
+        })
+    }else if(orderStatus === "cart"){
+      return await Order.findAll({
+        where:{
+          userId:userId,
+          orderStatus:"cart"
+        },
+        include:[
+          {
+            model: OrderLine,
+          },
+        ]
+      }).then((order)=>{
+        console.log(order)
+        if(order.length < 1){
+          let newOrder={
+            userId:newUserId,
+            total:0,
+            orderStatus: "cart"
+          }
+          return Order.create(newOrder).then((order) => order);
+        }
+        return order
+      }).catch((error)=>{
+        return error
+      })
+    }else{
+      return await Order.findAll({
+        where:{
+          userId:userId,
+          orderStatus:orderStatus
+        },
+        include:[
+          {
+            model: OrderLine,
+          },
+        ]
+      }).then((orders)=>{
+        return orders
+      }).catch((error)=>{
+        return error
+      })
+    }
+
+  }
 };
