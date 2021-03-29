c2b033d1bb6a54502aba91634256ef4c27d2043e
Feli8712 <felipe.traina.azul@gmail.com>
2021-03-28 22:42:29 -0300

    USER ACTIONS SOMES FIX AND CLEAN CODE

--- client/src/stores/user/user.actions.js
+++ client/src/stores/user/user.actions.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
-//FETCH USER FROM THE SERVER WITCH THUNK
+//FETCH USER 
 export const GET_USER_REQUEST = "GET_USER_REQUEST";
 export const GET_USER_SUCCESS = "GET_USER_SUCCESS";
 export const GET_USER_FAILURE = "GET_USER_FAILURE";
-//FETCH CART FROM THE SERVER WITCH THUNK
+//FETCH CART 
 export const GET_CART_REQUEST = "GET_CART_REQUEST";
 export const GET_CART_SUCCESS = "GET_CART_SUCCESS";
 export const GET_CAR_FAILURE = "GET_CAR_FAILURE";
@@ -15,8 +15,8 @@ export const ADD_ITEM_TO_CARD_FAILURE = "ADD_ITEM_TO_CARD_FAILURE";
 export const DELETE_ITEM_CART_REQUEST = "DELETE_ITEM_CART_REQUEST";
 export const DELETE_ITEM_CART_SUCCESS = "DELETE_ITEM_CART_SUCCESS";
 export const DELETE_ITEM_CART_FAILURE = "DELETE_ITEM_CART_FAILURE";
+//DISCOUNT
 export const GET_CALCULATOR_TOTAL_PRICE = "GET_CALCULATOR_TOTAL_PRICE";
-//COUPON
 export const GET_DISCOUNT_COUPON = "GET_DISCOUNT_COUPON";
 
 //GET USER
@@ -34,115 +34,95 @@ export const fetchUser = () => {
             },
             headers: {'Content-Type': 'application/x-www-form-urlencoded'},
           };
-        axios.request(options)
-            .then(user => {
+
+        axios.request(options).then(user => {
                 
                 setTimeout(()=>{
-                    dispatch(getUserSuccess(user))
-                    dispatch(fetchCart({userState:true, id:user.data.user.id}))
-                },100)
+                    dispatch(getUserSuccess(user));
+                    dispatch(fetchCart({userState:true, id:user.data.user.id}));
+                },100);
                 
-            })
-            .catch(error => {
-                dispatch(getUserFailure(error))
-                dispatch(fetchCart({userState:false}))
-            })
-    }
-}
+            }).catch(error => {
+                dispatch(getUserFailure(error));
+                dispatch(fetchCart({userState:false}));
+            });
+    };
+};
 
 export const getUserRequest = () =>{
     return {
         type: GET_USER_REQUEST,
-    }
-} 
+    };
+}; 
 export const getUserSuccess = (user) =>{
     return {
         type:GET_USER_SUCCESS,
         payload: user
-    }
-} 
+    };
+}; 
 export const getUserFailure = (error) =>{
     return {
         type:GET_USER_FAILURE,
         payload: error
-    }
-} 
+    };
+}; 
 
 //GET CALCULATOR TOTAL PRICE
 export const getCalculator = () =>{
     return {
         type:GET_CALCULATOR_TOTAL_PRICE,
-    }
-} 
+    };
+}; 
 
 //GET CART
 export const fetchCart = (user) => {
 
     return (dispatch) =>{
+
     if(user.userState){
-            dispatch(getCartRequest())
+            
+        dispatch(getCartRequest());
+
             axios.get(`http://localhost:3001/order/user/${user.id}`)
                 .then(cart => {
-                    dispatch(getCartSuccess(user, cart.data[0]))
-                    console.log("AHORA USERRRRR",cart)
+                    dispatch(getCartSuccess(user, cart.data[0]));
                 })
                 .catch(error => {
-                    dispatch(getCartFailure(error))
-                    console.log("AHORA USERRRRR",error.message)
+                    dispatch(getCartFailure(error));
                 })
         
         }else{
-            let localCart =  localStorage.getItem('localCart')
+            let localCart = localStorage.getItem('localCart');
             if(localCart){
-                dispatch(getCartSuccess(false, JSON.parse(localCart)))
+                dispatch(getCartSuccess(false, JSON.parse(localCart)));
             }else{
                 localStorage.setItem('localCart', "[]");
-            }
-           // dispatch(getCartSuccess(false, JSON.parse(localCart)))
-        }
-    }
-    
-    /*
-    else{
-        return (dispatch)=>{
-            let testCart =[
-                {
-                    id: 7,
-                    orderStatus: "cart",
-                    total: "0",
-                    userId: 36580834,
-                    orderLines: ["test Cart Local Store"]
-                  }
-            ]
-            dispatch(getCartSuccess(user, testCart));
-            dispatch(getCalculator());
-            
-        }
-    }
-    */
-}
+            };
+        };
+    };
+};
 
 export const getCartRequest = () =>{
     return {
         type: GET_CART_REQUEST,
-    }
-} 
+    };
+}; 
 export const getCartSuccess = (user, cart) =>{
     let obj ={
         user, 
         cart
-    }
+    };
     return {
         type:GET_CART_SUCCESS,
         payload: obj
-    }
+    };
 } 
 export const getCartFailure = (error) =>{
     return {
         type:GET_CAR_FAILURE,
         payload: error
-    }
-} 
+    };
+};
 
 //ADD ITEM TO CART
 export const addItemToCart = (user, product) => {
@@ -223,20 +203,20 @@ export const addItemToCart = (user, product) => {
 export const addItemToCartRequest = () =>{
     return {
         type: ADD_ITEM_TO_CARD_REQUEST,
-    }
-} 
+    };
+};
 export const addItemToCartSuccess = (product) =>{
     return {
         type:ADD_ITEM_TO_CARD_SUCCESS,
         payload: product
-    }
-} 
+    };
+};
 export const addItemToCartFailure = (error) =>{
     return {
         type:ADD_ITEM_TO_CARD_FAILURE,
         payload: error
-    }
-} 
+    };
+};
 
 //COUPON CODE----------------------------------------FALTA USER
 export const getDiscountCoupon = (code) => {
@@ -253,8 +233,8 @@ export const getDiscountCouponAction = (code) =>{
     return {
         type: GET_DISCOUNT_COUPON,
         payload: code
-    }
-} 
+    };
+};
 
 //DELETE ITEM CART
 export const deleteItemInCart = (orderLineId, user, idProduct) => {
@@ -269,11 +249,9 @@ export const deleteItemInCart = (orderLineId, user, idProduct) => {
                 url: `http://localhost:3001/orderline/${orderLineId}`,
               };
          return await axios.request(options).then(response => {
-                    console.log("RESPONSE DELETE",response)
                     dispatch(fetchCart(user));
                 }).catch(error => {
-                    console.log("RESPONSE DELETE",error)
-                    dispatch(getCartFailure(error))
+                    dispatch(getCartFailure(error));
                 })          
         }else{
             var auxCart = [];
@@ -285,44 +263,43 @@ export const deleteItemInCart = (orderLineId, user, idProduct) => {
 
                     for (let i = 0; i < LocalCart.length; i++) {
                         if(LocalCart[i].productId === idProduct){
-                            console.log("IGUALES", LocalCart[i].productId, " / ", idProduct)
+                            console.log("IGUALES", LocalCart[i].productId, " / ", idProduct);
  
                          }else{
                               auxCart.push(LocalCart[i]);
-                         }       
-                       
+                         };    
                     };
+
                     localStorage.setItem('localCart', JSON.stringify(auxCart));
                     dispatch(fetchCart(false));
                 
-                }
+                };
             };
-        }
-      
-    }
-}
+        };
+    };
+};
 
 export const deleteItemCartRequest = () =>{
     return {
         type: DELETE_ITEM_CART_REQUEST,
-    }
-} 
+    };
+};
 export const deleteItemCartSuccess = (productId, deleteAll) =>{
     let obj = {
         productId,
         deleteAll
-    }
+    };
     return {
         type:DELETE_ITEM_CART_SUCCESS,
         payload: obj
-    }
-} 
+    };
+};
 export const deleteItemCartFailure = (error) =>{
     return {
         type:DELETE_ITEM_CART_FAILURE,
         payload: error
-    }
-} 
+    };
+};
 
 //DELETE ALL ITEM CART
 export const deleteAllItemInCart = (user,orderId) => {
@@ -337,12 +314,11 @@ export const deleteAllItemInCart = (user,orderId) => {
          return await axios.request(options).then(() => {
                     dispatch(fetchCart(user));
                 }).catch(error => {
-                    dispatch(getCartFailure(error))
+                    dispatch(getCartFailure(error));
                 })          
         }else{
             localStorage.setItem('localCart', JSON.stringify([]));
             dispatch(fetchCart(false));
-        }
-      
-    }
-}
+        };
+    };
+};
