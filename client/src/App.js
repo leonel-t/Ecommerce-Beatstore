a3d017cc2922bc345cd752c9fc85148c6ca2f54c
leonel-t <tomasini.leonel@gmail.com>
2021-04-05 00:31:41 -0300

    commit prev merge

--- client/src/App.js
+++ client/src/App.js
@@ -69,18 +69,8 @@ const App = () => {
     <BrowserRouter>
       <Switch>
         <RouteWrapper exact path="/" component={Home} layout={HomeLayout} />
-        <RouteWrapper
-          exact
-          path="/catalog"
-          component={Catalog}
-          layout={PagesLayout}
-        />
-        <RouteWrapper
-          exact
-          path="/product/:productId"
-          component={Product}
-          layout={ProductLayout}
-        />
+        <RouteWrapper exact path="/catalog" component={Catalog} layout={PagesLayout}/>
+        <RouteWrapper exact path="/product/:productId" component={Product} layout={ProductLayout}/>
         <RouteWrapper exact path="/payment" component={CheckoutPay} layout={CheckoutPay} />
         <RouteWrapper exact path="/admin/listproducts" component={ProductList} layout={AdminLayout} />
         <RouteWrapper exact path="/listcat" component={ListCategories} layout={AdminLayout} />
@@ -102,13 +92,10 @@ const App = () => {
         <RouteWrapper exact path="/profile/edit" component={EditUserProfile} layout={ProfileLayout} />
         <RouteWrapper path="/profile/order/:id" component={OrderDetails} layout={PagesLayout} />
         <RouteWrapper exact path="/login/github/:email/:password" component={LoginGithub} layout={PagesLayout} />
-
         <RouteWrapper exact path="/checkout" component={Checkout} layout={PagesLayout}/>
-
-
         <RouteWrapper exact path="/oferts" component={Oferts} layout={PagesLayout} />
         <RouteWrapper exact path="/ranking" component={Ranking} layout={PagesLayout} />
-
+        
         <RouteWrapper exact path="/adminsubscriptions" component={AdminSubscriptions} layout={AdminSubscriptionsLayout} />
         <RouteWrapper exact path="/adminsuscriptions/customers" component={Customers} layout={AdminSubscriptionsLayout} />
         <RouteWrapper exact path="/adminsuscriptions/products" component={SuscriptionsProducts} layout={AdminSubscriptionsLayout} />
