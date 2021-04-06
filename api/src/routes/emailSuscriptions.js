c4dd4db427389afe177515b0a1e4f71e813220ba
zaratush <martinc.1@hotmail.com>
2021-04-06 17:39:59 -0300

    works ok

--- api/src/routes/emailSuscriptions.js
+++ api/src/routes/emailSuscriptions.js
@@ -1,14 +1,10 @@
-/*
-    Video: https://www.youtube.com/watch?v=Va9UKGs1bwI
-    Don't forget to disable less secure app from Gmail: https://myaccount.google.com/lesssecureapps TODO:
-*/
 
 require('dotenv').config();
-const { User } = require("../db");
-
+const { Newsletter } = require("../db");
 const nodemailer = require('nodemailer');
+const hbs = require('nodemailer-express-handlebars');
 const log = console.log;
-
+let emailList = []
 // Step 1
 let transporter = nodemailer.createTransport({
     service: 'gmail',
@@ -17,29 +13,43 @@ let transporter = nodemailer.createTransport({
         pass: process.env.PASSWORD || '1234' // TODO: your gmail password
     }
 });
+// transporter.use('compile', hbs({
+//     viewEngine: 'express-handlebars',
+//     viewPath: './src/views'
+// }));
+
 module.exports = {
 
 
     sendMail: function () {
 
-        var mailist = ['martinc.1@hotmail.com', 'martincavanaghx@gmail.com', 'martinzaratustra@gmail.com']
-        // Step 2
-        for (let i = 0; i < mailist.length; i++) {
-            let mailOptions = {
-                from: 'asdf', // TODO: email sender
-                to: mailist[i], // TODO: email receiver
-                subject: 'Nodemailer - Test',
-                text: 'Wooohooo it works!!'
-            };
+        Newsletter.findAll({
+            attributes: ["email"],
+            raw: true
+        }).then(list => {
+            emailList = list.map(email => email.email)
+            log(emailList)
+            for (let i = 0; i < emailList.length; i++) {
+                let mailOptions = {
+                    from: 'asdf', // TODO: email sender
+                    to: emailList[i], // TODO: email receiver
+                    subject: 'Nodemailer - Test',
+                    template: 'index',
+                    text: "here are the newsletters",
+                    // context: {
+                    //     name: 'beatstars'
+                    // }
+                };
 
-            // Step 3
-            transporter.sendMail(mailOptions, (err, data) => {
-                if (err) {
-                    return log('Error occurs');
-                }
-                return log('Email sent!!!');
-            });
-        }
+                // Step 3
+                transporter.sendMail(mailOptions, (err, data) => {
+                    if (err) {
+                        return log('Error occurs' + err);
+                    }
+                    return log('Email sent!!!');
+                });
+            }
+        })
     }
 
 }
