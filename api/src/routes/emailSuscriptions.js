fa0ce185c0ba73f8d795732a624b3a1cc254a234
zaratush <martinc.1@hotmail.com>
2021-04-06 20:01:13 -0300

    fix

--- api/src/routes/emailSuscriptions.js
+++ api/src/routes/emailSuscriptions.js
@@ -2,8 +2,9 @@
 require('dotenv').config();
 const { Newsletter } = require("../db");
 const nodemailer = require('nodemailer');
-const hbs = require('nodemailer-express-handlebars');
+const hbs = require('nodemailer-handlebars');
 const log = console.log;
+const path = require("path");
 let emailList = []
 // Step 1
 let transporter = nodemailer.createTransport({
@@ -13,42 +14,46 @@ let transporter = nodemailer.createTransport({
         pass: process.env.PASSWORD || '1234' // TODO: your gmail password
     }
 });
-// transporter.use('compile', hbs({
-//     viewEngine: 'express-handlebars',
-//     viewPath: './src/views'
-// }));
+transporter.use('compile', hbs(
+    {
+        viewEngine: {
+            partialsDir: "./views",
+            defaultLayout: ""
+        },
+        viewPath: "./views",
+        extName: ".hbs"
+    }
 
+));
 module.exports = {
 
+    sendMail: async function () {
 
-    sendMail: function () {
-
-        Newsletter.findAll({
+        await Newsletter.findAll({
             attributes: ["email"],
             raw: true
         }).then(list => {
             emailList = list.map(email => email.email)
             log(emailList)
-            for (let i = 0; i < emailList.length; i++) {
-                let mailOptions = {
-                    from: 'asdf', // TODO: email sender
-                    to: emailList[i], // TODO: email receiver
-                    subject: 'Nodemailer - Test',
-                    template: 'index',
-                    text: "here are the newsletters",
-                    // context: {
-                    //     name: 'beatstars'
-                    // }
-                };
+            let mailOptions = {
+                from: 'asdf', // TODO: email sender
+                to: emailList, // TODO: email receiver
+                subject: 'Nodemailer - Test',
+                template: 'index',
+                text: "here are the newsletters",
+                context: {
+                    name: 'beatstars'
+                }
+            };
 
-                // Step 3
-                transporter.sendMail(mailOptions, (err, data) => {
-                    if (err) {
-                        return log('Error occurs' + err);
-                    }
-                    return log('Email sent!!!');
-                });
-            }
+            // Step 3
+            transporter.sendMail(mailOptions, (err, data) => {
+                if (err) {
+                    return log('Error occurs' + err);
+                }
+                return log('Email sent!!!');
+            });
+
         })
     }
 
