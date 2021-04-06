f173c31b6dac3bd223cd2a70b25a58a0cad5eeb5
Feli8712 <felipe.traina.azul@gmail.com>
2021-04-06 06:38:09 -0300

    ORDERS WIDGET COMPLETE

--- client/src/pages/Admin/AdminDashboard/OrdersWidget/OrdersWidget.js
+++ client/src/pages/Admin/AdminDashboard/OrdersWidget/OrdersWidget.js
@@ -1,31 +1,145 @@
 import "./orderwidget.scss";
-import React from "react";
+import React, {useEffect} from "react";
+import {connect} from "react-redux";
+import {withTranslation} from "react-i18next";
+import {fetchAllOrders} from "../../../../stores/admin/admin.actions";
+import spinner from "../../../../assets/images/Spin-1s-200px.svg"
+const OrderWidget = ({fetchAllOrdersEffect, STORE_ADMIN})=>{
 
-const OrderWidget = ()=>{
+    useEffect(() => {
+        fetchAllOrdersEffect()
+    }, [fetchAllOrdersEffect])
+
     return (
         <>
         <div className="--order-widget-title">
             <h1>Orders Live</h1>
         </div>
+        {STORE_ADMIN.ordersLoading
+        ?(
+            <img src={spinner} alt="loading..."></img>
+        ):(
         <div className="--order-widget-main">
             <div className="--order-widget-col-cart">
-                <p>Cart</p>
+                <div>
+                    cart
+                </div>
+                <div>
+                    {STORE_ADMIN.orders && STORE_ADMIN.orders.length > 0
+                        ?(
+                            STORE_ADMIN.orders.filter(order=>{
+                                if(order.orderStatus === "cart"){
+                                    return order
+                                }
+                                return ""
+                            }).length
+
+                        ):(
+                            <p>Not Orders</p>
+                        )
+                    }
+                </div>
             </div>
             <div className="--order-widget-col-create">
-                <p>Create</p>
+                <div>
+                    create
+                </div>
+                <div>
+                    {STORE_ADMIN.orders && STORE_ADMIN.orders.length > 0
+                        ?(
+                            STORE_ADMIN.orders.filter(order=>{
+                                if(order.orderStatus === "create"){
+                                    return order
+                                }
+                                return ""
+                            }).length
+
+                        ):(
+                            <p>Not Orders</p>
+                        )
+                    }
+                </div>
             </div>
             <div className="--order-widget-col-process">
-                <p>Process</p>
+                <div>
+                    process
+                </div>
+                <div>
+                    {STORE_ADMIN.orders && STORE_ADMIN.orders.length > 0
+                        ?(
+                            STORE_ADMIN.orders.filter(order=>{
+                                if(order.orderStatus === "process"){
+                                    return order
+                                }
+                                return ""
+                            }).length
+
+                        ):(
+                            <p>Not Orders</p>
+                        )
+                    }
+                </div>
             </div>
             <div className="--order-widget-col-cancel">
-                <p>Cancel</p>
+                <div>
+                    cancel
+                </div>
+                <div>
+                    {STORE_ADMIN.orders && STORE_ADMIN.orders.length > 0
+                        ?(
+                            STORE_ADMIN.orders.filter(order=>{
+                                if(order.orderStatus === "cancel"){
+                                    return order
+                                }
+                                return ""
+                            }).length
+
+                        ):(
+                            <p>Not Orders</p>
+                        )
+                    }
+                </div>
             </div>
             <div className="--order-widget-col-complete">
-                <p>Complete</p>
+                <div>
+                    complete
+                </div>
+                <div>
+                    {STORE_ADMIN.orders && STORE_ADMIN.orders.length > 0
+                        ?(
+                            STORE_ADMIN.orders.filter(order=>{
+                                if(order.orderStatus === "complete"){
+                                    return order
+                                }
+                                return ""
+                            }).length
+
+                        ):(
+                            <p>Not Orders</p>
+                        )
+                    }
+                </div>
             </div>
         </div>
+        )
+
+        }
+
+
         </>
     )
 };
 
-export default OrderWidget
+const mapStateToProps = (state) => {
+    return {
+      STORE_ADMIN: state.adminReducers,
+    };
+  };
+  
+  const mapDispatchToProps = (dispatch) => {
+    return {
+        fetchAllOrdersEffect: () => dispatch(fetchAllOrders()),
+    };
+  };
+  
+ export default connect(mapStateToProps, mapDispatchToProps)(withTranslation()(OrderWidget));
