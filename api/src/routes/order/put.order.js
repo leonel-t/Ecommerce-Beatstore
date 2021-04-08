059b2442c4c1821345c071d25cbbf4ffa82f0b87
leonel-t <tomasini.leonel@gmail.com>
2021-04-08 15:48:20 -0300

    fix discount

--- api/src/routes/order/put.order.js
+++ api/src/routes/order/put.order.js
@@ -1,5 +1,6 @@
 const server = require("express").Router();
 const { editOrder } = require("../../controllers/order/order.controller");
+const {Order} = require("../../db");
 
 server.put("/:orderId", (req, res) => {
   const { orderId } = req.params;
@@ -10,6 +11,21 @@ server.put("/:orderId", (req, res) => {
     })
     .catch((error) => {
       res.status(400).json(error);
+    });
+});
+server.put("/discount/:IdOrder", async (req, res, next) => {
+  const {IdOrder} = req.params
+  const {orderDiscount} = req.body
+  return await Order.findByPk(IdOrder).then(async (order)=>{
+    return await Order.update({discount: orderDiscount}, {
+      where:{
+        id:IdOrder,
+      }
+    }).then((order)=>{
+      return res.status(200).json(order)
+    })
+  }).catch((error) => {
+      return error
     });
 });
 
