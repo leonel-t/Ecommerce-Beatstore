ba48c6e284575bcd072c74be03b96c1ea7b8b93e
leonel-t <tomasini.leonel@gmail.com>
2021-04-08 21:09:50 -0300

    arreglao el order detail

--- client/src/components/Profile/OrderDetails/OrderDetails.js
+++ client/src/components/Profile/OrderDetails/OrderDetails.js
@@ -5,20 +5,21 @@ import { connect } from "react-redux";
 import { useParams } from 'react-router';
 import {serverUrl} from '../../../auxiliar/variables';
 
-const OrderDetails = ({ order }) => {
+const OrderDetails = ({ USER_ORDERS }) => {
     const { id } = useParams();
-    console.log("la id es" + id)
-    const totalPrice = order[0].orderLines.map(order => parseFloat(order.price))
+    // const totalPrice = order[0].orderLines.map(order => parseFloat(order.price))
+    const order = USER_ORDERS.find((order)=>parseInt(order.id) === parseInt(id))
+    const totalPrice = order.orderLines.map(order => parseFloat(order.price))
 
 
     return (
         <div>
             <div className="--Orders">
                 <div className="--Orders">
-                    {order && order.length > 0 ? (
+                    {order ? (
                         <>
                             <div>
-                                {order[0].orderLines.map((product, index) => {
+                                {order.orderLines.map((product, index) => {
                                     return (
                                         <div key={index} className="--ItemCard">
                                             <div className="--ItemCard-left">
@@ -43,7 +44,7 @@ const OrderDetails = ({ order }) => {
                                     );
                                 })}
 
-                                <h1 className="total" style={{color: "white",fontFamily: 'Oxanium'}}>Total price:
+                                <h1 className="total" style={{color: "white",fontFamily: 'Oxanium'}}>Total price: $
                                     {
                                         totalPrice && totalPrice.length > 0 ? (
                                             totalPrice.reduce((accumulator, currentValue) => {
@@ -71,7 +72,7 @@ const OrderDetails = ({ order }) => {
 };
 const mapStateToProps = (state) => {
     return {
-        order: state.userReducers.orders,
+        USER_ORDERS: state.userReducers.orders,
     };
 };
 
