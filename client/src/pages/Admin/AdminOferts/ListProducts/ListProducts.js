c52d7b98faf0c5c5afa8e5d5b7ca1eb0063e9aba
Feli8712 <felipe.traina.azul@gmail.com>
2021-04-09 00:05:48 -0300

    ADD OFERTS STYLES UPDATE

--- client/src/pages/Admin/AdminOferts/ListProducts/ListProducts.js
+++ client/src/pages/Admin/AdminOferts/ListProducts/ListProducts.js
@@ -17,16 +17,15 @@ const ListProducts = ({id, name, image, artist, description, addOfert})=>{
         <>
         <div className="--admin-list-main">
             <div className="--admin-list-main-img">
-                <img width="150px" src={`${serverUrl}/images/${image}`} alt="User ph"/>
+                <img width="100px" height="100px" src={`${serverUrl}/images/${image}`} alt="User ph"/>
             </div>
             <div className="--admin-list-main-details">
                 <p>Id : {id}</p>
-                <p>name : {name}</p>
-                <p>artist : {artist}</p>
-                <p>description : {description}</p>
+                <p>{name}</p>
             </div >
-            <div>
+            <div className="--admin-list-main-btn">
                 <button
+                className="--admin-list-main-btn-button"
                 onClick={handleClick}
                 >Add Ofert</button>
             </div>
