5cc0e9b2a9c0e7adf3f947deeb8da9a4e29dd29f
leonel-t <tomasini.leonel@gmail.com>
2021-06-15 18:55:44 -0300

    before upload

--- client/src/auxiliar/variables.js
+++ client/src/auxiliar/variables.js
@@ -1,3 +1,3 @@
 export const serverUrl = "http://localhost:3001"
 export const clientUrl = "http://localhost:3000"
-export const STRIPE_KEY = "pk_test_51IeDNfHCtxTXo0iYY43T8urU27CLBCcBMb8Xav6uu8UL04YowCz5jLjSTLKqXIeUmbrM1uWLqvobeaieOO5o9Mqw00NIYqrpk4"
+export const STRIPE_KEY = "pk_test_51IbFjrLDJyVvtDkgSdbcQBERHyS60JKwwgP1txRVecZIQaA268HyHtWB9o285SwM1H9A1EhoUL7DMU6iKgIzlqbk00wBHvgczU"
